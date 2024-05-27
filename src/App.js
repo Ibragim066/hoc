332cf70be3563712e9db310c50e7076d37834bdf
Ibragim <gamemega.ao@gmail.com>
2024-05-27 10:29:52 +0300

    implemented logic

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 import React from "react";
+import { List, VideoList } from "./copmonents";
+import { list, videoList } from "./data";
+
+import "./App.css";
+
+const App = () => (
+  <>
+    <h2>Task #1 Time</h2>
+    <div className="container">
+      <VideoList list={videoList} />
+    </div>
+
+    <h2>Task #2 Highlight</h2>
+    <div className="container">
+      <List list={list} />
+    </div>
+  </>
+);
+
+export default App;
